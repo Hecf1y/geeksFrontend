d9a46effd10cbdac7bedce4c9186e55ee50a9e77
TAURUS <102017137+Hecf1y@users.noreply.github.com>
2026-07-08 19:00:46 +0600

    dz_1

---
+++ homework/2nd_month/dz_1/app.js
@@ -0,0 +1,39 @@
+let name = "SpongeBob";
+let surName = "SquarePants";
+let age = 40;
+let fullName_Bob = `${name} ${surName} ${age} year(s)`;
+
+const name2 = "Sandy";
+const surName2 = "Cheeks";
+const age2 = 36;
+const fullName_Sandy = `${name2} ${surName2} ${age2} year(s)`;
+
+var name3 = "Gary";
+var surName3 = "the Snail";
+var age3 = 2;
+var fullName_Gary = `${name3} ${surName3} ${age3} year(s)`;
+
+console.log(fullName_Bob);
+console.log(fullName_Sandy);
+console.log(fullName_Gary);
+
+
+
+let x = 10;
+let y = 9;
+let z = 0;
+
+console.log(x > x, x == x, x > y, x < y,);
+console.warn(x*y);
+
+let t = true ;
+let f = false;
+
+console.log(t+f);
+console.log(t-f);
+console.log(t>f);
+console.log(t<f);
+console.log(t==t);
+
+console.log(t&&f, t>t);
+
